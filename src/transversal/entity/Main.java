a082684db570c8e307bac1b6c5f6c571330c8b3a
FiorellaAilen <fiorellaailen@gmail.com>
2023-10-10 18:51:40 -0300

    Entrega 1 relleno de clases

--- src/transversal/entity/Main.java
+++ src/transversal/entity/Main.java
@@ -1,11 +1,9 @@
-
 package transversal.entity;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
-    
 }
--- src/tranversal/access/Conexion.java
+++ src/tranversal/access/Conexion.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tranversal.access;
 
-/**
- *
- * @author Usuario
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "transversal";
+    private static final String usuario = "root";
+    private static final String password = "";
+    private static Connection conexion;
+
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+
+        if (conexion == null) {
+
+            try {
+                //Cargo Driver de conexión
+                Class.forName("org.mariadb.jdbc.Driver");
+
+                conexion = DriverManager.getConnection(URL + DB + usuario + password);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar Driver" + ex.getMessage());
+
+            } catch (SQLException ex) {
+//                int error = ex.getErrorCode();
+//                if (error == 1146) {
+//                    JOptionPane.showMessageDialog(null, "Tabla inexistente");
+//
+//                } else if (error == 1049) {
+//
+//                    JOptionPane.showMessageDialog(null, "BD inexistente");
+//                } else {
+
+                JOptionPane.showMessageDialog(null, "Error SQL" + ex.getMessage());
+
+            }
+        }
+        return conexion;
+
+    }
 }
