71c5b80d75197fd99404c3a5b1589463ad2b68b9
Coni <coti.garibotto@gmail.com>
2023-10-18 18:03:52 -0300

    update Pruebas Main.

--- src/transversal/AccesoADatos/InscripcionData.java
+++ src/transversal/AccesoADatos/InscripcionData.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package transversal.AccesoADatos;
 
-/**
- *
- * @author Usuario
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import transversal.Entidades.Inscripcion;
+
+
 public class InscripcionData {
+    private Connection con = null;
     
+    public InscripcionData(){
+    con = Conexion.getConexion();
+    }
+    
+    public void guardarInscripcion (Inscripcion insc){
+    
+        String sql = "INSERT inscripcion(idAlumno, idMateria, nota) VALUES (?,?,?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        
+            ps.setInt(1,insc.getAlumno().getIdAlumno());
+            ps.setInt(2, insc.getMateria().getIdMateria());
+            ps.setDouble(3, insc.getNota());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()) {
+               insc.setIdInscripcion(rs.getInt(1));
+               JOptionPane.showMessageDialog(null, "Inscripcion Registrada.");
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion.");
+        }
+        
+    }
 }
