80f2063948f0b83a417c705e720570d9bf446ae9
Coni <coti.garibotto@gmail.com>
2023-10-19 13:16:41 -0300

    update InscripcionData.

--- src/transversal/AccesoADatos/InscripcionData.java
+++ src/transversal/AccesoADatos/InscripcionData.java
@@ -19,27 +19,49 @@ public class InscripcionData {
     con = Conexion.getConexion();
     }
     
-    public void guardarInscripcion (Inscripcion insc){
-    
-        String sql = "INSERT inscripcion(idAlumno, idMateria, nota) VALUES (?,?,?)";
+    public void guardarInscripcion(Inscripcion insc) {
+
+        String sql = "INSERT INTO inscripcion(idAlumno, idMateria, nota) VALUES (?,?,?) ";
         try {
-            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        
-            ps.setInt(1,insc.getAlumno().getIdAlumno());
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, insc.getAlumno().getIdAlumno());
             ps.setInt(2, insc.getMateria().getIdMateria());
             ps.setDouble(3, insc.getNota());
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
-            if(rs.next()) {
-               insc.setIdInscripcion(rs.getInt(1));
-               JOptionPane.showMessageDialog(null, "Inscripcion Registrada.");
+            if (rs.next()) {
+                insc.setIdInscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Inscripcion Registrada.");
             }
-            
+
             ps.close();
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion.");
         }
+
+       
+
+    public void actualizarNota(int idAlumno, int idMateria, double nota) {
+        
+        String sql = "UPDATE inscripcion SET nota = ? WHERE idAlumno = ? and idMateria = ? ";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setDouble(1, nota);
+            ps.setInt(2, idAlumno);
+            ps.setInt(3, idMateria);
+           
+            int filas=ps.executeUpdate();
+            if(filas>0){
+                JOptionPane.showMessageDialog(null, "Nota actualizada");
+            }
+         ps.close();
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion.");
+        }
         
     }
+
 }
+
