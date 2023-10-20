c4fcf08ab9c2650789f21ad0086886fecb66b764
Coni <coti.garibotto@gmail.com>
2023-10-20 15:54:21 -0300

    update  corrección alumno

--- src/transversal/Entidades/Alumno.java
+++ src/transversal/Entidades/Alumno.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 
 public class Alumno {
 
-    int idAlumno;
-    int dni;
-    String apellido;
-    String nombre;
-    LocalDate fechaNac;
-    boolean activo;
+   private int idAlumno;
+   private int dni;
+   private String apellido;
+   private String nombre;
+   private LocalDate fechaNac;
+   private boolean activo;
 
     public Alumno() {
     }
